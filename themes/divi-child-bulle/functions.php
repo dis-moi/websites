f3655ad70244f8b793e43107fadfd61ca962f179
Graeme Hoffman <graeme@graemehoffman.com>
2019-04-05 14:30:56 +0200

    Child Divi theme base

---
+++ themes/divi-child-bulle/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Enqueue parent styles
+ */
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );

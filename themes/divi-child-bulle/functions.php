057eafed3b92ce7ad9504270db7a68cc63b57f32
Graeme Hoffman <graeme@graemehoffman.com>
2019-06-17 11:49:29 +0200

    Allow SVG files

--- themes/divi-child-bulle/functions.php
+++ themes/divi-child-bulle/functions.php
@@ -49,3 +49,10 @@ function output_bulle_overlay() {
 }
 add_action( 'wp_footer', 'output_bulle_overlay' );
 
+
+function cc_mime_types($mimes) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
